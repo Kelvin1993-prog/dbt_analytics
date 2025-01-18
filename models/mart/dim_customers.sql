8d99242d46113704234b870808ac3d5e2acb39a3
kelvin <kelvinbullet1993@gmail.com>
2025-01-18 16:42:07 +0000

    building both intermediate and mart layer

---
+++ models/mart/dim_customers.sql
@@ -0,0 +1,9 @@
+WITH customers AS (
+    SELECT * FROM {{ ref("stg_jaffle_shop_customers") }}
+)
+
+SELECT 
+    customers.customer_id,
+    customers.first_name,
+    customers.last_name
+FROm customers 
---
+++ models/mart/fct_orders.sql
@@ -0,0 +1,23 @@
+WITH orders AS (
+    SELECT * FROM {{ ref("stg_jaffle_shop_orders") }}
+),
+
+payment_type_orders AS (
+    SELECT * FROM {{ ref("int_payment_type_amount_per_orders") }}
+)
+
+SELECT 
+    o.order_id,
+    o.customer_id,
+    o.order_date,
+    pt.cash_amount,
+    pt.credit_amount,
+    pt.total_amount,
+    CASE 
+        WHEN status = 'completed'
+        THEN 1
+        ELSE 0
+    END AS is_order_completed
+FROM orders AS o 
+LEFT JOIN payment_type_orders AS pt
+ON o.order_id = pt.order_id 
