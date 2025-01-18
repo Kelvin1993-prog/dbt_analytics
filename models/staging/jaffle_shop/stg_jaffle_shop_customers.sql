1909f47c511615c009d6261ba86012e062223833
kelvin <kelvinbullet1993@gmail.com>
2025-01-18 15:34:05 +0000

    made changes in customers table from view to table

--- models/staging/jaffle_shop/stg_jaffle_shop_customers.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop_customers.sql
@@ -1,5 +1,13 @@
 -- loading the customer table from the source
 
+-- let us materialize our customer data as a table in dbt
+
+{{
+    config(
+        materialized = 'table'
+    )
+}}
+
 SELECT 
     id AS customer_id,
     first_name,
